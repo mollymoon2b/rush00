847c7b4b19291118b54c063a0a3cf7e5a47172ec
Alice DE-BONNECHOSE <ade-bonn@e2r2p18.42.fr>
2015-04-09 19:23:25 +0200

    bien ranger

--- inscription.php
+++ inscription.php
@@ -1,38 +1,26 @@
 <?php
  	session_start();
 
-		if(isset($_POST['submit'])){
-					$username = $_POST['username'];
-					$password = $_POST['password'];
-
-					if ($username&&$password){
+	if(isset($_POST['submit'])){
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$delimiter = ':';
+			if ($username&&$password){
 					 
-						$data = array(array($line, $username, $password)); //$Line++ pour modif les id
-						$line++;
-						if ($f = @fopen('bdd/user.csv', 'a=')) {
-						  foreach ($data as $ligne) {
-						    fputcsv($f, $ligne);
-						    }
-						  fclose($f);
-						  }
-						else {
-						  echo "Impossible d'acc&eacute;der au fichier.";
-						  }
-					}else{
-						echo "Veuillez remplir tous les champs";
-					}
-					}			
+				$data = array(array($line, $username, $password)); //$Line++ pour modif les id
+				$line++;
+				if ($f = @fopen('bdd/user.csv', 'a=')) {
+					 foreach ($data as $ligne) {
+					   fputcsv($f, $ligne, $delimiter);
+					   }
+				  fclose($f);
+				  }
+			else {
+				 echo "Impossible d'acc&eacute;der au fichier.";
+				  }
+			}else{
+				echo "Veuillez remplir tous les champs";
+			}
+		}
+		require_once('includes/inscription.html');
 ?>
-<html>
-<head>
-
-</head>
-<body>
-	<h1>Administration - Connexion</h1>
-	<form action="" method="POST">
-		<h3>Pseudo :</h3> <input type="text" name="username"> <br>
-		<h3>Mot de pass :</h3> <input type="password" name="password"> <br>
-		<input type="submit" name="submit">
-	</form>
-	</body>
-</html>
